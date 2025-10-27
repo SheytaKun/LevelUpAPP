ca9ea0277b7875616f8d7803b9cc8d7246a86a1d
PAAPCA03LC0526 <Duoc@PAAPCA03LC0526.duoc.acad>
2025-10-27 20:13:22 -0300

    Se agrego el color, theme, type del caso

--- app/src/main/java/com/example/levelup/ui/theme/Type.kt
+++ app/src/main/java/com/example/levelup/ui/theme/Type.kt
@@ -1,34 +1,47 @@
 package com.example.levelup.ui.theme
 
+import androidx.compose.material3.R
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+private val provider = Provider(
+    authority = "com.google.android.gms.fonts",
+    packageName = "com.google.android.gms",
+    certificates = R.font.google_fonts_certs
+)
+
+private val orbitron = GoogleFont("Orbitron")
+private val roboto = GoogleFont("Roboto")
+
+val AppTypography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = GoogleFontTypeface(orbitron, provider),
+        fontSize = 34.sp,
+        lineHeight = 38.sp,
+        color = White
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    headlineMedium = TextStyle(
+        fontFamily = GoogleFontTypeface(orbitron, provider),
+        fontSize = 24.sp,
+        lineHeight = 28.sp,
+        color = White
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = GoogleFontTypeface(roboto, provider),
+        fontSize = 16.sp,
+        lineHeight = 22.sp,
+        color = White
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = GoogleFontTypeface(roboto, provider),
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        color = LightGray
+    ),
+    labelLarge = TextStyle(
+        fontFamily = GoogleFontTypeface(roboto, provider),
+        fontSize = 14.sp
+    ),
 )
